953133a10c4459e4218900ac2d7172e36a1fd550
WoodsGeo442 <44784501+WoodsGeo442@users.noreply.github.com>
2020-03-15 22:08:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 app.use(express.static('css'));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
@@ -11,23 +9,46 @@ app.get("/", function(req, res){
     res.render("index.html");
 });
 
+app.get("/home", function(req, res){
+    res.render("index.html");
+});
+
 app.get("/mercury", function(req, res){
     res.render("mercury.html");
 });
 
 app.get("/venus", function(req, res){
-    res.send("This will be Venus web page!");
+    res.render("venus.html");
+});
+
+app.get("/earth", function(req, res){
+    res.render("earth.html");
+});
+
+app.get("/mars", function(req, res){
+    res.render("mars.html");
+});
+
+app.get("/jupiter", function(req, res){
+    res.render("jupiter.html");
+});
+
+app.get("/saturn", function(req, res){
+    res.render("saturn.html");
+});
+
+app.get("/uranus", function(req, res){
+    res.render("uranus.html");
+});
+
+app.get("/neptune", function(req, res){
+    res.render("neptune.html");
 });
 
 /* The handler for undefined routes */
 app.get("*", function(req, res){
    res.send("<h2 style='color: red;'> Sorry, Page not found !</h2>"); 
 });
-
-// //server listener
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("Running Express Server...");
-// });
 
 /* Start the application server */
 app.listen(process.env.PORT || 3000, function(){
